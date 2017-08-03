d68efea76d670ea6cf2003ea186a02a6d26356ca
Vishal Patel <vishalpatel2890@gmail.com>
2017-08-03 19:45:54 -0400

    something

--- recipeapi/urls.py
+++ recipeapi/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from api import views
-from api.views import RecipeNamesList, IngredientsList, StepsList
+from api.views import RecipeNamesList, IngredientsList, StepsList, IngredientsViewSet
 
 router = routers.DefaultRouter()
 router.register(r'recipes', views.RecipesViewSet)
-router.register(r'ingredients', views.IngredientsViewSet)
+# router.register(r'ingredients', views.IngredientsViewSet)
 router.register(r'steps', views.StepsViewSet)
 router.register(r'user', views.UserViewSet)
- 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -17,6 +16,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^recipesearch/(?P<recipename>.+)/$', RecipeNamesList.as_view()),
     url(r'^ingredientsearch/(?P<recipeid>.+)/$', IngredientsList.as_view()),
-    url(r'^stepsearch/(?P<recipeid>.+)/$', StepsList.as_view())
+    url(r'^stepsearch/(?P<recipeid>.+)/$', StepsList.as_view()),
+    url(r'^ingredients/$', IngredientsViewSet.as_view()),
 
 ]
